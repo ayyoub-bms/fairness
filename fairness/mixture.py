5f76c9e26963613e2d7759c262e6596480ea17c7
ayyoub-bms <>
2023-06-02 20:47:50 +0200

    add Bayes and estimation

--- fairness/mixture.py
+++ fairness/mixture.py
@@ -1,8 +1,9 @@
 import numpy as np
+from numpy import ndarray
 from scipy.stats import multivariate_normal, norm
 
 
-def simulate(n, d, p, p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
+def generate_samples(n, d, p, p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
     """Simulate the following data
         S ~ B(p)
         Y | S = 1 ~ B(p1)
@@ -40,7 +41,8 @@ def simulate(n, d, p, p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
 
     Returns
     -------
-
+        a tuple (features, target, sensitive) of random samples
+        from the mixture model.
     """
     s = np.random.choice(2, n, p=(1 - p, p))
     y = np.zeros(n, dtype='int')
@@ -54,72 +56,107 @@ def simulate(n, d, p, p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
     y[~s_mask] = np.random.choice(2, n - m, p=(1 - p0, p0))
     y_mask = (y == 1)
 
-    mask11 = y_mask & s_mask
-    mask01 = ~y_mask & s_mask
-    mask10 = y_mask & ~s_mask
-    mask00 = ~y_mask & ~s_mask
+    mask11 = np.where(y_mask & s_mask)[0]
+    mask01 = np.where(~y_mask & s_mask)[0]
+    mask10 = np.where(y_mask & ~s_mask)[0]
+    mask00 = np.where(~y_mask & ~s_mask)[0]
 
-    x[mask11] = multivariate_normal(mu11, cov1).rvs(sum(mask11)).reshape(-1, d)
-    x[mask01] = multivariate_normal(mu01, cov1).rvs(sum(mask01)).reshape(-1, d)
-    x[mask10] = multivariate_normal(mu10, cov0).rvs(sum(mask10)).reshape(-1, d)
-    # noinspection PyTypeChecker
-    x[mask00] = multivariate_normal(mu00, cov0).rvs(sum(mask00)).reshape(-1, d)
+    x[mask11] = multivariate_normal(mu11, cov1).rvs(len(mask11))
+    x[mask01] = multivariate_normal(mu01, cov1).rvs(len(mask01))
+    x[mask10] = multivariate_normal(mu10, cov0).rvs(len(mask10))
+    x[mask00] = multivariate_normal(mu00, cov0).rvs(len(mask00))
+
     return x, s, y
 
 
-def eta0(x, p0, mu00, mu10, cov0):
-    """ Returns the regression fuction
-
-    .. math::
-        \\eta(x, 0)
-
-    Parameters
-    ----------
-    x: Array like
-        The random vector realization
-    p0: float
-        The conditional probability of Y = 1 given S = 0
-    mu00: Numpy 1D array of shape (d, 1)
-        represents the mean vector of the rv: X | S=0, Y=0
-    mu10: Numpy 1D array of shape (d, 1)
-        represents the mean vector of the rv: X | S=0, Y=1
-    cov0: Numpy 2D array of shape (d, d)
-        The covariance matrix of the conditional distribution X|Y, S=0
-    Returns
-    -------
-        the regression function (posterior/class probabilities)
-        for the non-sensitive group
-    """
-    return _dist(x, p0, mu00, mu10, cov0)
+def estimate(X, y, s):
+    mu00, mu01, mu10, mu11 = _class_means(X, y, s)
+    cov0, cov1 = _class_cov(X, s)
+    p0 = y[np.where(s == 0)[0]].mean()
+    p1 = y[np.where(s == 1)[0]].mean()
+    r = regression_function(X, s, p0, p1, mu00, mu01, mu10, mu11, cov0, cov1)
+    return r, (r > .5).astype(int)
 
 
-def eta1(x, p1, mu01, mu11, cov1):
+def regression_function(X, s, p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
     """ Returns the regression fuction
 
         .. math::
-            \\eta(x, 1)
+            \\eta(x, s)
 
         Parameters
         ----------
-        x: Array like
-            The random vector realization
+        X: Numpy 2D array  of shape (n, d)
+            The random samples
+        s: Numpy 1D array of shape (n, 1)
+            The samples of the sensitive feature
+        p0: float
+            The conditional probability of Y = 1 given S = 0
         p1: float
             The conditional probability of Y = 1 given S = 1
+        mu00: Numpy 1D array of shape (d, 1)
+            represents the mean vector of the rv: X | S=0, Y=0
+        mu10: Numpy 1D array of shape (d, 1)
+            represents the mean vector of the rv: X | S=0, Y=1
         mu01: Numpy 1D array of shape (d, 1)
             represents the mean vector of the rv: X | S=1, Y=0
         mu11: Numpy 1D array of shape (d, 1)
             represents the mean vector of the rv: X | S=1, Y=1
+        cov0: Numpy 2D array of shape (d, d)
+            The covariance matrix of the conditional distribution X|Y, S=0
         cov1: Numpy 2D array of shape (d, d)
             The covariance matrix of the conditional distribution X|Y, S=0
         Returns
         -------
-            the regression function (posterior/class probabilities)
-            for the sensitive group
+            The regression function (posterior/class probabilities)
+            for the sensitive and non-sensitive group
         """
-    return _dist(x, p1, mu01, mu11, cov1)
+    reg_function = np.zeros(X.shape[0])
+    s_mask = s == 1
+    reg_function[~s_mask] = _dist(X[~s_mask], p0, mu00, mu10, cov0)
+    reg_function[s_mask] = _dist(X[s_mask], p1, mu01, mu11, cov1)
+    return reg_function
 
 
-def unfairness(p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
+def bayes_classifier(X, s, p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
+    """ Returns the bayes classifier
+
+        .. math::
+            g^\\star(x, s) = \\mathbb{1}_{\\eta(x, s) \\geq 1/2}
+
+        Parameters
+        ----------
+        X: Numpy 2D array  of shape (n, d)
+            The random samples
+        s: Numpy 1D array of shape (n, 1)
+            The samples of the sensitive feature
+        p0: float
+            The conditional probability of Y = 1 given S = 0
+        p1: float
+            The conditional probability of Y = 1 given S = 1
+        mu00: Numpy 1D array of shape (d, 1)
+            represents the mean vector of the rv: X | S=0, Y=0
+        mu10: Numpy 1D array of shape (d, 1)
+            represents the mean vector of the rv: X | S=0, Y=1
+        mu01: Numpy 1D array of shape (d, 1)
+            represents the mean vector of the rv: X | S=1, Y=0
+        mu11: Numpy 1D array of shape (d, 1)
+            represents the mean vector of the rv: X | S=1, Y=1
+        cov0: Numpy 2D array of shape (d, d)
+            The covariance matrix of the conditional distribution X|Y, S=0
+        cov1: Numpy 2D array of shape (d, d)
+            The covariance matrix of the conditional distribution X|Y, S=0
+
+        Returns
+        -------
+            A numpy array of ints corresponding to classes of
+            the Bayes classifier for the  sensitive and non-sensitive group
+        """
+    r = regression_function(X, s, p0, p1, mu00, mu01, mu10, mu11, cov0, cov1)
+    return (r > .5).astype(int)
+
+
+def equal_opportunity(p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
     """ Computs the unfairness using the Equal Opportunity measure:
 
     .. math::
@@ -173,60 +210,30 @@ def unfairness(p0, p1, mu00, mu01, mu10, mu11, cov0, cov1):
                   norm.cdf(x0, loc=mu0, scale=np.sqrt(s0)))
 
 
-def g0(x, p0, mu00, mu10, cov0):
-    """ Returns the bayes classifier
-
-    .. math::
-        g^\\star(x, 0) = \\mathbb{1}_{\\eta(x, 0) \\geq 1/2}
-
-    Parameters
-    ----------
-    x: Array like
-        The random vector realization
-    p0: float
-        The conditional probability of Y = 1 given S = 0
-    mu00: Numpy 1D array of shape (d, 1)
-        represents the mean vector of the rv: X | S=0, Y=0
-    mu10: Numpy 1D array of shape (d, 1)
-        represents the mean vector of the rv: X | S=0, Y=1
-    cov0: Numpy 2D array of shape (d, d)
-        The covariance matrix of the conditional distribution X|Y, S=0
-
-    Returns
-    -------
-        int or an array of ints corresponding to classes of
-        the Bayes classifier for the non-sensitive group
-    """
-    return int(eta0(x, p0, mu00, mu10, cov0) > .5)
-
-
-def g1(x, p1, mu01, mu11, cov1):
-    """ Returns the bayes classifier for the sensitive group
-
-    .. math::
-        g^\\star(x, 1) = \\mathbb{1}_{\\eta(x, 1) \\geq 1/2}
-
-    Parameters
-    ----------
-    x: Array like
-        The random vector realization
-    p1: float
-        The conditional probability of Y = 1 given S = 1
-    mu01: Numpy 1D array of shape (d, 1)
-        represents the mean vector of the rv: X | S=1, Y=0
-    mu11: Numpy 1D array of shape (d, 1)
-        represents the mean vector of the rv: X | S=1, Y=1
-    cov1: Numpy 2D array of shape (d, d)
-        The covariance matrix of the conditional distribution X|Y, S=0
-    Returns
-    -------
-        int or an array of ints corresponding to classes of
-        the Bayes classifier for the sensitive group
-    """
-    return int(eta1(x, p1, mu01, mu11, cov1) > .5)
-
-
 def _dist(x, p, mu0, mu1, cov):
     f1 = multivariate_normal(mu1, cov).pdf(x)
     f0 = multivariate_normal(mu0, cov).pdf(x)
     return p * f1 / (p * f1 + (1 - p) * f0)
+
+
+def _class_means(X, y, s):
+    y_mask = y == 1
+    s_mask = s == 1
+
+    mask11 = np.where(y_mask & s_mask)
+    mask01 = np.where(~y_mask & s_mask)
+    mask10 = np.where(y_mask & ~s_mask)
+    mask00 = np.where(~y_mask & ~s_mask)
+
+    mu00 = np.mean(X[mask00], axis=0)
+    mu01 = np.mean(X[mask01], axis=0)
+    mu10 = np.mean(X[mask10], axis=0)
+    mu11 = np.mean(X[mask11], axis=0)
+    return mu00, mu01, mu10, mu11
+
+
+def _class_cov(X, s):
+    s_mask = s == 1
+    cov1 = np.cov(X[s_mask].T)
+    cov0 = np.cov(X[~s_mask].T)
+    return cov0, cov1
